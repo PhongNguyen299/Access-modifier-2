a575dd60347e4ee8de9baa2644294ed0ea486157
PhongNguyen299 <siphongnguyen.hcm@gmail.com>
2022-12-08 23:30:51 +0700

    Initital

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setName("Phong");
+        student1.setClasses("C10");
+        System.out.println(student1.display());
+    }
+}
